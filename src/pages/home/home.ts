b92a34607dc7edbab343379edf475ffb81f292f4
731646594 <731646594@qq.com>
2018-11-23 16:52:33 +0800

    0.0.2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,28 @@ import {StorageService} from "../../services/storageService";
 })
 export class HomePage {
   user;
+  todoList=[];
+  inventoryNum=0;
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService) {
-    this.user=this.storageService.read("loginInfo")[0].user;
-    this.user["depart"]=this.storageService.read("loginDepart");
+    this.loadData()
   }
   ionViewDidEnter(){
+    this.todoList=[];
+    this.loadData();
+  }
+  loadData(){
     this.user=this.storageService.read("loginInfo")[0].user;
     this.user["depart"]=this.storageService.read("loginDepart");
+    this.httpService.post(this.httpService.getUrl()+"toDoController.do?tododetailcounts", {userCode:this.user.usercode,departCode:this.user.depart.departcode}).subscribe((data)=>{
+      if (data.success=="true"){
+        let i=0;
+        for (let key in data.data[0]) {
+          this.todoList.push([key,data.data[0][key]]);
+        }
+        console.log(this.todoList)
+      }
+    },err=>{
+      console.log(err)
+    })
   }
 }
