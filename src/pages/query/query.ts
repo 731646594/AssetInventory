6d362a2d740f742d35b40ad66ecbd58ffe816bd6
731646594 <731646594@qq.com>
2018-12-14 16:36:03 +0800

    0.2.21修复部分bug,加入echart插件

--- src/pages/query/query.ts
+++ src/pages/query/query.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {AlertController, App, NavController, NavParams} from 'ionic-angular';
+import {AlertController, App, NavController, NavParams, ToastController} from 'ionic-angular';
 import {HttpService} from "../../services/httpService";
 import {StorageService} from "../../services/storageService";
 import {BarcodeScanner, BarcodeScannerOptions} from "@ionic-native/barcode-scanner";
 import {File} from "@ionic-native/file";
+import * as  echarts from 'echarts';
 @Component({
   selector: 'page-query',
   templateUrl: 'query.html'
@@ -30,10 +31,13 @@ export class QueryPage {
   departName;
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService,
               public app:App,public navParams:NavParams,public barcodeScanner:BarcodeScanner, public alertCtrl:AlertController,
-              public file:File) {
+              public file:File,public toastCtrl:ToastController) {
     this.loadData();
   }
   ionViewDidEnter(){
+    if (this.pageIndex==3){
+      this.drawChart()
+    }
   }
   loadData(){
     this.user=this.storageService.read("loginInfo")[0].user;
@@ -44,10 +48,58 @@ export class QueryPage {
     }
     this.departCode = this.user.depart.departcode;
     this.departName = this.user.depart.departname;
-    if (this.pageIndex==2&&this.storageService.read("departListData")){
+    if ((this.pageIndex==2||this.pageIndex==3)&&this.storageService.read("departListData")){
       this.departListData = this.storageService.read("departListData");
       this.lastDepartListData = this.departListData;
     }
+    if (this.pageIndex==3){
+
+    }
+  }
+  drawChart(){
+    const ec = echarts as any;
+    const container = document.getElementById('chart');
+    console.log(container);
+    const chart = ec.init(container);
+    let option = {
+
+      tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis : [
+        {
+          type : 'category',
+          data : ['Mon', 'Tue'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis : [
+        {
+          type : 'value'
+        }
+      ],
+      series : [
+        {
+          name:'直接访问',
+          type:'bar',
+          barWidth: '60%',
+          data:[{value:330,itemStyle:{color:'blue'}}, 220]
+        }
+      ]
+    };
+    chart.setOption(option);
+
   }
   inputOnfocus(){
     this.isOnfocus=true;
@@ -108,6 +160,7 @@ export class QueryPage {
         }
       })
     }else if (this.pageIndex == 2){
+      this.page=1;
       url = "cellPhoneController.do?queryLedgerList";
       if (!this.userPerson){
         this.userPerson = ""
@@ -117,6 +170,7 @@ export class QueryPage {
         console.log(data)
         if (data.success == "true"){
           this.detail = data.data;
+          this.detail["count"] = data.count;
         }else {
           alert(data.msg)
         }
@@ -126,7 +180,34 @@ export class QueryPage {
     }
 
   }
-
+  getMore(infiniteScroll){
+    this.page++;
+    let url,body;
+    url = "cellPhoneController.do?queryLedgerList";
+    if (!this.userPerson){
+      this.userPerson = ""
+    }
+    body = {departCode:this.departCode,assetsType:this.assetsType,userPerson:this.userPerson,page:this.page,pageSize:this.pageSize};
+    this.httpService.post(this.httpService.getUrl()+url,body).subscribe(data=>{
+      console.log(data)
+      if (data.success == "true"){
+        for (let i in data.data){
+          this.detail.push(data.data[i])
+        }
+      }else {
+        alert(data.msg)
+      }
+      if (!data.data[0]){
+        infiniteScroll.enable(false);
+        let toast = this.toastCtrl.create({
+          message: "这已经是最后一页了",
+          duration: 2000,
+        });
+        toast.present();
+      }
+      infiniteScroll.complete();
+    })
+  }
   selectDepart(departName){
     this.departName = departName;
   }
