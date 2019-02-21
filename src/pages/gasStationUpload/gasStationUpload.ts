5eca75ef0c860319392f127761889e838a365a34
731646594 <731646594@qq.com>
2019-02-21 13:20:57 +0800

    0.3.4数据库存储加油站测试4

--- src/pages/gasStationUpload/gasStationUpload.ts
+++ src/pages/gasStationUpload/gasStationUpload.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {AlertController, App, NavController, NavParams} from 'ionic-angular';
+import {AlertController, App, LoadingController, NavController, NavParams} from 'ionic-angular';
 import {HttpService} from "../../services/httpService";
 import {StorageService} from "../../services/storageService";
+import {PhotoViewer} from "@ionic-native/photo-viewer";
 
 @Component({
   selector: 'page-gasStationUpload',
@@ -13,12 +14,19 @@ export class GasStationUploadPage {
   zjb=[];
   jjb=[];
   item=[];
+  colItem=[];
   itemName;
   checkedIndex;
   uploadList=new Array(2);
+  colsItemName=[];
+  photoArrary=[];
+  signatureImage1;
+  signatureImage2;
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService,
-              public alertCtrl:AlertController,public navParams:NavParams,public app:App) {
+              public alertCtrl:AlertController,public navParams:NavParams,public app:App,public loadingCtrl:LoadingController,
+              public photoViewer:PhotoViewer) {
     this.user=this.storageService.read("loginInfo")[0].user;
+    this.user["depart"]=this.storageService.read("loginDepart");
     this.pageIndex = navParams.get("pageIndex");
     if (this.pageIndex==1){
       this.storageService.getUserTable().executeSql('SELECT * FROM zjb WHERE userCode=\''+this.user.usercode+'\';',[]).then(res =>{
@@ -33,12 +41,44 @@ export class GasStationUploadPage {
       }).catch(e =>alert("erro22:"+JSON.stringify(e))  )
     }
     else if (this.pageIndex==2){
+      let url;
       if (navParams.get("name")=="zjb"){
         this.itemName = "zjb";
+        url = "devWeeklyCheckController.do?getCheckListCols";
       }else if (navParams.get("name")=="jjb"){
         this.itemName = "jjb";
+        url = "devHandOverController.do?getCheckListCols";
       }
       this.item = navParams.get("data");
+      for(let i in this.item){
+        if (i.indexOf("col")!=-1){
+          this.colItem.push(this.item[i])
+        }
+      }
+      let photoLen = this.item["uploadFile"].length;
+      this.signatureImage1 = this.item["uploadFile"][photoLen-2];
+      this.signatureImage2 = this.item["uploadFile"][photoLen-1];
+      this.photoArrary = this.item["uploadFile"].splice(photoLen-2,2);
+      let loading = this.loadingCtrl.create({
+        content:"请等待...",
+        duration: 10000
+      });
+      loading.present();
+      this.httpService.post(this.httpService.getUrl()+url,{departCode:this.user["depart"]["departcode"]}).subscribe(data=>{
+        if (data.success=="true"){
+          let colsData = data.data.colsData;
+          for (let i in colsData){
+            for (let j in colsData[i]["fields"]){
+              this.colsItemName.push(colsData[i]["fields"][j].columnTitle)
+            }
+          }
+          loading.dismiss();
+        }else {
+          alert(data.msg);
+          loading.dismiss();
+        }
+      });
+
     }
   }
   ionViewDidEnter(){
@@ -61,5 +101,8 @@ export class GasStationUploadPage {
     if(data.length!=0){
       this.app.getRootNav().push(GasStationUploadPage,{data:data,name:name,pageIndex:2})
     }
+  }
+  showSign(imgData){
+    this.photoViewer.show(imgData);
   }
 }
